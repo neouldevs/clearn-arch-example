0d2c910dce1e16397d601a37c347ee998b55eb42
hyeyoom <neoul_chw@icloud.com>
2019-12-14 17:47:39 +0900

    20191214 - (1) UseCase를 정의함

--- module-usecase/src/main/java/com/github/hyeyoom/usecase/core/todo/CreateTodoUseCase.java
+++ module-usecase/src/main/java/com/github/hyeyoom/usecase/core/todo/CreateTodoUseCase.java
@@ -1,4 +1,23 @@
 package com.github.hyeyoom.usecase.core.todo;
 
-public class CreateTodoUseCase {
+import com.github.hyeyoom.core.Todo;
+import com.github.hyeyoom.usecase.core.UseCase;
+
+public class CreateTodoUseCase implements UseCase<Todo, Void> {
+
+    private final TodoRepository repository;
+
+    public CreateTodoUseCase(TodoRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Void execute(Todo todo) {
+        repository.save(todo);
+        return null;
+    }
+
+    public interface TodoRepository {
+        void save(Todo todo);
+    }
 }
--- module-usecase/src/main/java/com/github/hyeyoom/usecase/core/todo/GetTodoByIdUseCase.java
+++ module-usecase/src/main/java/com/github/hyeyoom/usecase/core/todo/GetTodoByIdUseCase.java
@@ -1,4 +1,20 @@
 package com.github.hyeyoom.usecase.core.todo;
 
-public class GetTodoByIdUseCase {
+import com.github.hyeyoom.core.Todo;
+import com.github.hyeyoom.usecase.core.UseCase;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class GetTodoByIdUseCase implements UseCase<String, Todo> {
+
+    private final TodoRepository repository;
+
+    @Override
+    public Todo execute(String id) {
+        return repository.findById(id);
+    }
+
+    public interface TodoRepository {
+        Todo findById(String id);
+    }
 }
